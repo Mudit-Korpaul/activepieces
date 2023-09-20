e25fb465eb6af8aa1287cb5c8cec8d521b5637a6
Kishan Parmar <code.kishanparmar@gmail.com>
2023-09-20 10:57:32 +0530

    feat(acumbamail): add piece auth

--- packages/pieces/acumbamail/src/index.ts
+++ packages/pieces/acumbamail/src/index.ts
@@ -1,12 +1,24 @@
+import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
 
-import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+const markdown = `
+To obtain your API key, follow these steps:
+
+1. Login to your Acumbamail account.
+2. Go to https://acumbamail.com/apidoc/
+3. Under **Customer identifier**, you can see your auth token`;
+
+export const acumbamailAuth = PieceAuth.SecretText({
+  displayName: 'API Key',
+  required: true,
+  description: markdown,
+});
 
 export const acumbamail = createPiece({
-  displayName: "Acumbamail",
-  auth: PieceAuth.None(),
+  displayName: 'Acumbamail',
+  auth: acumbamailAuth,
   minimumSupportedRelease: '0.8.0',
-  logoUrl: "https://cdn.activepieces.com/pieces/acumbamail.png",
-  authors: [],
+  logoUrl: 'https://cdn.activepieces.com/pieces/acumbamail.png',
+  authors: ['kishanprmr'],
   actions: [],
   triggers: [],
 });
