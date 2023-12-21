a294c199d6a29b9bfada8cfc9ab07b0b3e398571
Kishan Parmar <code.kishanparmar@gmail.com>
2023-12-21 11:36:02 +0530

    feat(close-crm): add piece auth

--- packages/pieces/close-crm/src/index.ts
+++ packages/pieces/close-crm/src/index.ts
@@ -1,11 +1,20 @@
+import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
 
-import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
+export const closeCrmAuth = PieceAuth.SecretText({
+  displayName: 'API Key',
+  required: true,
+  description: `
+  1.Log in to your Close CRM account.
+  2.Navigate to **Settings->Developer->New API Key**.
+  3.Enter a name for the API key and click Save.
+  4.Copy the API Key to your clipboard.`,
+});
 
 export const closeCrm = createPiece({
-  displayName: "Close-crm",
-  auth: PieceAuth.None(),
+  displayName: 'Close-crm',
+  auth: closeCrmAuth,
   minimumSupportedRelease: '0.9.0',
-  logoUrl: "https://cdn.activepieces.com/pieces/close-crm.png",
+  logoUrl: 'https://cdn.activepieces.com/pieces/close-crm.png',
   authors: [],
   actions: [],
   triggers: [],
